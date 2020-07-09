98cdf26ffd4ce5a6a7bc83acde9cac15d6f7fb9e
3noix <nic.trannois@free.fr>
2020-07-09 14:16:10 +0200

    Initialisation de la solution Visual Studio

---
+++ Foodtruck/FoodtruckSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodtruckSite.Startup))]
+namespace FoodtruckSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
